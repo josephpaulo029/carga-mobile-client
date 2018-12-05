3c75eb8227ef785542ad95211a16da4104ea2d8f
warjohn123 <warrencaruana1@gmail.com>
2018-12-05 14:58:07 +0800

    Add maps in package item

--- src/pages/package-item/package-item.ts
+++ src/pages/package-item/package-item.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavParams } from 'ionic-angular';
 import { DeliveryService } from '../../providers/delivery.api';
+import { Geolocation } from '@ionic-native/geolocation';
+import { DeviceSocket } from '../../providers/devicesocket.service';
 import { Storage } from '@ionic/storage';
+import {
+    GoogleMaps,
+    GoogleMap,
+    LatLng,
+    LatLngBounds
+} from '@ionic-native/google-maps';
 
 @IonicPage()
 @Component({
@@ -14,13 +22,54 @@ export class PackageItemPage {
     deliveryId: any;
     package: any = {};
     isLoading: Boolean = false;
+    map: GoogleMap;
+    markers: any = [{}, {}];
+    markersCopy: any = [{}, {}];
+    lat: any;
+    lng: any;
+
     constructor(public navParams: NavParams, 
         private storage: Storage,
+        private deviceSocket: DeviceSocket,
+        private geolocation: Geolocation,
         private deliveryService: DeliveryService) {
     }
 
-    ionViewWillEnter() {
+    ionViewDidLoad() {
         this.deliveryId = this.navParams.get('deliveryId');
+        this.storage.get('currentLocation').then( value => {
+            if(value) {
+                this.map = GoogleMaps.create('map', {
+                    camera: {
+                        target: {
+                            lat: value.lat,
+                            lng: value.lng
+                        },
+                        zoom: 5
+                    }
+                });
+            } else {
+                this.map = GoogleMaps.create('map', {
+                    camera: {
+                        target: {
+                            lat: 12.8797,
+                            lng: 121.7740
+                        },
+                        zoom: 5
+                    }
+                });
+            }
+        }).catch( () => {
+            this.map = GoogleMaps.create('map', {
+                camera: {
+                    target: {
+                        lat: 12.8797,
+                        lng: 121.7740
+                    },
+                    zoom: 5
+                }
+            });
+        });
         this.getDelivery();
     }
 
@@ -30,7 +79,111 @@ export class PackageItemPage {
             this.deliveryService.getOne(this.deliveryId, token).subscribe( data => {
                 this.isLoading = false;
                 this.package = data['data'];
+
+                this.loadMap();
+
+                let index = this.markersCopy.findIndex( vehicle => vehicle.deviceId == this.package.vehicleInfo.pairedDevice.deviceId);
+
+                if(index != -1) {
+                    this.markers[1].setPosition({
+                        lat: this.package.vehicleInfo.currentGPSLocation.latitude,
+                        lng: this.package.vehicleInfo.currentGPSLocation.longitude
+                    });
+                } else {
+                    let marker = this.map.addMarkerSync({
+                        icon: 'assets/imgs/marker.png',
+                        position: {
+                            lat: this.package.vehicleInfo.currentGPSLocation.latitude,
+                            lng: this.package.vehicleInfo.currentGPSLocation.longitude
+                        }
+                    });
+                    this.markers[1] = marker;
+                    this.markersCopy[1] = {deviceId: this.package.vehicleInfo.pairedDevice.deviceId};
+                }
+                let coordinates: LatLng = new LatLng(this.package.vehicleInfo.currentGPSLocation.latitude, this.package.vehicleInfo.currentGPSLocation.longitude);
+                this.map.setCameraTarget(coordinates);
+                this.map.setCameraZoom(13);
             });
+        });
+    }
+
+    loadMap() {
+        this.geolocation.getCurrentPosition().then( response => {
+            this.lat = response.coords.latitude;
+            this.lng = response.coords.longitude;
+
+            this.storage.set('currentLocation', {
+                lat: this.lat,
+                lng: this.lng
+            });
+
+            let coordinates: LatLng = new LatLng(this.lat, this.lng);
+            
+            let marker = this.map.addMarkerSync({
+                icon: 'assets/imgs/user-marker.png',
+                position: {
+                    lat: this.lat,
+                    lng: this.lng
+                }
+            });
+            this.markers[0] = marker;
+
+            this.initDevices();
+            this.initWebSockets();
+        });
+
+        let watch = this.geolocation.watchPosition();
+        watch.subscribe( data => {
+            this.lat = data.coords.latitude;
+            this.lng = data.coords.longitude;
+
+            this.storage.set('currentLocation', {
+                lat: this.lat,
+                lng: this.lng
+            });
+
+            this.markers[0].setPosition({
+                lat: this.lat,
+                lng: this.lng
+            });
+        });
+    }
+
+    initDevices() {
+        this.deviceSocket.emit('subscribe', {
+            topic: '/device/' + this.package.ownerId + '/pub/' + this.package.vehicleInfo.pairedDevice.deviceId
+        });
+    }
+
+    initWebSockets() {
+        this.deviceSocket.on('server-to-client', data => {
+            let splits = data.payload.split(',');
+
+            let lat = splits[0];
+            let lng = splits[1];
+
+            let deviceId = data.topic;
+            deviceId = deviceId.split('/');
+            deviceId = deviceId[4];
+
+            let index = this.markersCopy.findIndex( vehicle => vehicle.deviceId == deviceId);
+
+            if(index != -1) {
+                this.markers[1].setPosition({
+                    lat: lat,
+                    lng: lng
+                });
+            } else {
+                let marker = this.map.addMarkerSync({
+                    icon: 'assets/imgs/marker.png',
+                    position: {
+                        lat: lat,
+                        lng: lng
+                    }
+                });
+                this.markers[1] = marker;
+                this.markersCopy[1] = {deviceId: deviceId};
+            }
         });
     }
 
